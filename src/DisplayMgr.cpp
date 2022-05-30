495070bfe39691903a292ee37427b488d7f696d7
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-05-30 10:53:37 -0700

    no message

--- src/DisplayMgr.cpp
+++ src/DisplayMgr.cpp
@@ -1635,7 +1635,7 @@ void DisplayMgr::drawCANBusScreen1(modeTransition_t transition){
 			normalizeCANvalue(cachedProps[i+4].key, val2);
  		
 	// spread across 21 chars
-		sprintf( buffer , " %9s  %9s ", val1.c_str(), val2.c_str());
+		sprintf( buffer , "%9s %9s", val1.c_str(), val2.c_str());
 		_vfd.setCursor(0, row1 + (i * rowsize) + 8);
 		_vfd.writePacket( (const uint8_t*) buffer,21);
 	}

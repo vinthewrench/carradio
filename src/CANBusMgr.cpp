34f3eded027aa1e88910dc04b7c817ecf12e23fd
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-05-31 13:16:55 -0700

    no message

--- src/CANBusMgr.cpp
+++ src/CANBusMgr.cpp
@@ -179,16 +179,18 @@ bool CANBusMgr::getStatus(vector<can_status_t> & statsOut){
 			can_status_t stat;
 			stat.ifName = key;
 			
-			printf("gs: %s\n", key.c_str());
 			if(_lastFrameTime.count(key))
 				stat.lastFrameTime = _lastFrameTime[key];
 			
 			if(_packetCount.count(key))
 				stat.packetCount = _packetCount[key];
+			
 			stats.push_back(stat);
  		}
 		
 	}
+	statsOut = stats;
+	
 	return stats.size() > 0;;
 }
 

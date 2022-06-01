c98397327b2e599412703642448ab458ba0bd595
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-06-01 15:58:55 -0700

    no message

--- src/GPSmgr.cpp
+++ src/GPSmgr.cpp
@@ -346,8 +346,8 @@ void GPSmgr::processNMEA(){
 		pthread_mutex_lock (&_mutex);
 		memset((void*)&_lastVelocity, 0, sizeof(_lastVelocity));
 		_lastVelocity.isValid = _nmea.isValid();
-		_lastVelocity.heading = _nmea.getCourse() /10000.;
-		_lastVelocity.speed = _nmea.getSpeed();
+		_lastVelocity.heading = _nmea.getCourse();
+		_lastVelocity.speed = _nmea.getSpeed()/1000.;
 		_lastVelocity.timestamp = now;
 		pthread_mutex_unlock (&_mutex);
 

f977c460be00149f1bc96566cc5aa42b177ed16b
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-06-01 15:06:09 -0700

    no message

--- src/DisplayMgr.cpp
+++ src/DisplayMgr.cpp
@@ -1506,11 +1506,11 @@ void DisplayMgr::drawGPSScreen(modeTransition_t transition){
 		_vfd.printPacket("%-3s", v[0].c_str());
 		
 		_vfd.setCursor(col+20, row );
-		_vfd.printPacket("%8s", v[1].c_str());
+		_vfd.printPacket("%-8s", v[1].c_str());
 
 		row += 10;
 		_vfd.setCursor(col+30 - 6, row );
-		_vfd.printPacket("%8s", v[2].c_str());
+		_vfd.printPacket("%-8s", v[2].c_str());
 
 		row += 15;
 		if(location.altitudeIsValid)  {
@@ -1521,7 +1521,7 @@ void DisplayMgr::drawGPSScreen(modeTransition_t transition){
 			_vfd.setCursor(col+30, row );
 			_vfd.setFont(VFD::FONT_5x7);
 			constexpr double  M2FT = 	3.2808399;
-			_vfd.printPacket("%5.1f",location.altitude * M2FT);
+			_vfd.printPacket("%-5.1f",location.altitude * M2FT);
 		}
 
 	}

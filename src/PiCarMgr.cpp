692af67bc4d5db585e3956abd6c165bcf4cfb8e1
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-06-09 12:22:53 -0700

    no message

--- src/PiCarMgr.cpp
+++ src/PiCarMgr.cpp
@@ -594,16 +594,16 @@ bool PiCarMgr::nextKnownStation(RadioMgr::radio_mode_t band,
 	auto v = _stations[band];
 	// scan for next known freq
 	
-	printf("moved %s ", tunerMovedCW?"CW":"CCW");
+	printf("band %d moved %s ", band, tunerMovedCW?"CW":"CCW");
 	if(tunerMovedCW){
 		
 		for ( auto i = v.begin(); i != v.end(); ++i ) {
 			if(frequency < i->frequency)
 				continue;
-			
+		 
+			info =  *(i);
 			printf("selected  %d \n", info.frequency);
 
-			info = *i;
 			return true;;
 		}
 	}
@@ -612,8 +612,7 @@ bool PiCarMgr::nextKnownStation(RadioMgr::radio_mode_t band,
 		for ( auto i = v.rbegin(); i != v.rend(); ++i ) {
 			if(frequency > i->frequency)
 				continue;
-			info = *i;
-			
+			info =  *(i);
 			printf("selected  %d \n", info.frequency);
 
 			return true;;

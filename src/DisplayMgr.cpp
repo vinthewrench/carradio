dbf0bd13ed0227d967f579734d5a90c42bc93a20
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-06-12 13:34:27 -0700

    no message

--- src/DisplayMgr.cpp
+++ src/DisplayMgr.cpp
@@ -1739,23 +1739,23 @@ void DisplayMgr::drawCANBusScreen1(modeTransition_t transition){
 		
 	// Draw values
 	_vfd.setFont(VFD::FONT_5x7);
-	for(uint8_t	 i = start_item; i < end_item -3; i++){
-		
+	for(uint8_t	 i = start_item; i < end_item; i++){
+
 		char buffer[30];
 		memset(buffer, ' ', sizeof(buffer));
 		
 		string val1 = "";
 		string val2 = "";
  
-		if(cachedProps.count(i+1))
-			normalizeCANvalue(cachedProps[i+1].key, val1);
+		if(cachedProps.count(i))
+			normalizeCANvalue(cachedProps[i].key, val1);
 
-		if(cachedProps.count(i+4))
-			normalizeCANvalue(cachedProps[i+4].key, val2);
+		if(cachedProps.count(i+3))
+			normalizeCANvalue(cachedProps[i+3].key, val2);
  		
 	// spread across 21 chars
 		sprintf( buffer , "%-9s  %-9s", val1.c_str(), val2.c_str());
-		_vfd.setCursor(col1, row1 + (i * rowsize) + 9);
+		_vfd.setCursor(col1 ,(row1 + (i -1)  * rowsize) + 9);
 		_vfd.writePacket( (const uint8_t*) buffer,21);
 	}
  

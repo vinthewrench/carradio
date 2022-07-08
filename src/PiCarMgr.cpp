15e254810333d22f7a91c170e92c23e1befb4426
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-07-08 15:40:00 -0700

    no message

--- src/PiCarMgr.cpp
+++ src/PiCarMgr.cpp
@@ -1409,8 +1409,6 @@ void PiCarMgr::displaySettingsMenu(){
 
 void PiCarMgr::tunerDoubleClicked(){
 	DisplayMgr::mode_state_t dMode = _display.active_mode();
-
-	printf("tunerDoubleClicked mode = %d\n", dMode);
 	
 	if(dMode == DisplayMgr::MODE_RADIO
 		&& _radio.isOn()){

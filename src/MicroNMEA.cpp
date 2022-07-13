5094667b10d13fc308bcbdafadcba3dea29cd99d
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-07-13 16:49:09 -0700

    no message

--- src/MicroNMEA.cpp
+++ src/MicroNMEA.cpp
@@ -328,7 +328,36 @@ const char* MicroNMEA::parseDate(const char* s)
   return skipField(s + 6);
 }
 
+time_t mktimegm( struct tm * t)
+/* struct tm to seconds since Unix epoch */
+{
+	  long year;
+	  time_t result;
+#define MONTHSPERYEAR   12      /* months per calendar year */
+	 static const int cumdays[MONTHSPERYEAR] =
+		  { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 };
 
+	 /*@ +matchanyintegral @*/
+	 year = 1900 + t->tm_year + t->tm_mon / MONTHSPERYEAR;
+	 result = (year - 1970) * 365 + cumdays[t->tm_mon % MONTHSPERYEAR];
+	 result += (year - 1968) / 4;
+	 result -= (year - 1900) / 100;
+	 result += (year - 1600) / 400;
+	 if ((year % 4) == 0 && ((year % 100) != 0 || (year % 400) == 0) &&
+		  (t->tm_mon % MONTHSPERYEAR) < 2)
+		  result--;
+	 result += t->tm_mday - 1;
+	 result *= 24;
+	 result += t->tm_hour;
+	 result *= 60;
+	 result += t->tm_min;
+	 result *= 60;
+	 result += t->tm_sec;
+	 if (t->tm_isdst == 1)
+		  result -= 3600;
+	 /*@ -matchanyintegral @*/
+	 return (result);
+}
 void MicroNMEA::createTimeSpec(struct timespec &ts){
 	
 	struct tm t;
@@ -340,9 +369,9 @@ void MicroNMEA::createTimeSpec(struct timespec &ts){
 	t.tm_hour 	= _hour -2 ;
 	t.tm_min 	= _minute  ;
 	t.tm_sec		= _second;
-	t.tm_gmtoff	= 0;
+//	t.tm_gmtoff	= 8;
 	
-	ts.tv_sec =  mktime(&t);
+	ts.tv_sec =  mktimegm(&t);
 	ts.tv_nsec = _hundredths * 1e+7;
 	
 	

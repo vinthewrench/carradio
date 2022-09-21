21d523c6cb1d0694d42e46ceb6fe1ffe50ba0212
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-09-21 14:53:52 -0700

    no message

--- src/DisplayMgr.cpp
+++ src/DisplayMgr.cpp
@@ -3952,11 +3952,11 @@ void DisplayMgr::drawChannelInfo(modeTransition_t transition){
 		_vfd.printPacket("%-20s",channelStr.c_str() );
   
 		_vfd.setCursor(0, 60);
-		
+ 		_vfd.printPacket("           ");
+
 		bool isPreset = mgr->isPresetChannel(mode, freq);
 		bool isScanner = mgr->isScannerChannel(mode, freq);
-		
-		_vfd.printPacket("           ");
+		_vfd.setCursor(0, 60);
 		_vfd.setFont(VFD::FONT_MINI);
 		_vfd.printPacket("%s %s", isPreset?"PRESET":"", isScanner?"SCANNER":"");
   	}

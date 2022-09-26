610221dc2c051c7b35b2b2b3303d97a81fd2da71
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-09-25 17:41:15 -0700

    no message

--- src/DisplayMgr.cpp
+++ src/DisplayMgr.cpp
@@ -397,7 +397,7 @@ void DisplayMgr::ledEventSet(uint32_t set, uint32_t reset){
 	_ledEvent &= ~reset;
 	_ledEvent |= set;
 	
- 	printf("ledEventSet %08x %08x = %08x\n",set,reset,_ledEvent);
+// 	printf("ledEventSet %08x %08x = %08x\n",set,reset,_ledEvent);
 
 	pthread_mutex_unlock (&_led_mutex);
 	// only signal if you are setting a flag
@@ -432,8 +432,6 @@ void DisplayMgr::LEDUpdateLoop(){
 		while((_ledEvent & 0x0000ffff) == 0){
 			
 			// delay for half second
-			
-	
 #if 1
 			struct timespec ts = {0, 0};
 			clock_gettime(TIMEDWAIT_CLOCK, &ts);
@@ -454,7 +452,7 @@ void DisplayMgr::LEDUpdateLoop(){
 					printf( "LEDUpdateLoop: pthread_cond_timedwait : %s\n", strerror(result));
 				}
 				
-#if 1
+#if 0
 				// debugging how pthread_cond_timedwait works
 				struct timespec ts1 = {0, 0};
 				clock_gettime(TIMEDWAIT_CLOCK, &ts1);

9680d06922a1f89761491ed91cefea938572a49c
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-10-09 13:42:02 -0700

    no message

--- src/AudioLineInput.cpp
+++ src/AudioLineInput.cpp
@@ -139,10 +139,11 @@ bool AudioLineInput::getSamples(SampleVector& audio){
 		int cnt =  snd_pcm_readi(_pcm,  audio.data(), avail);
 		if(cnt > 0){
 			
-			double  mean,  rms;
-			samples_mean_rms(audio,  mean,  rms);
+			printf("frames: %ld\n", cnt/2);
+//			double  mean,  rms;
+//			samples_mean_rms(audio,  mean,  rms);
 
-			printf("m: %f rms: %f \n" , mean, rms);
+//			printf("m: %f rms: %f \n" , mean, rms);
 	 		return true;
 		}
 		

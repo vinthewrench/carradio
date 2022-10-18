ccf5008ef1f1ef831e968dcba97dcbdf1c4fcf50
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-10-18 10:32:01 -0700

    no message

--- src/VFD.cpp
+++ src/VFD.cpp
@@ -168,14 +168,15 @@ void VFD::drawScrollBar(uint8_t topbox,  float bar_height, float starting_offset
 	uint8_t  rightbox = this->width() -1;
 	uint8_t  leftbox = rightbox - 3;
 	uint8_t  bottombox = 63;
-	uint8_t  scroll_height = bottombox - topbox -2;
+	uint8_t  scroll_height = bottombox - topbox -1;
 	uint8_t  bar_size =  scroll_height * bar_height;
 	uint8_t  offset =  (scroll_height * starting_offset) + topbox +1;
 	
 	if((bar_size + offset) > bottombox)
 		bar_size = bottombox - offset;
 	
-	printf("drawScrollBar(%d,%.2f,%.2f)  offset = %d bar_size:%d\n", topbox,bar_height, starting_offset, offset, bar_size);
+	printf("drawScrollBar(%d,%.2f,%.2f)  offset = %d bar_size:%d scroll_height = %d\n",
+			 topbox,bar_height, starting_offset, offset, bar_size, scroll_height);
 	
 	uint8_t buff2[] = {
 		VFD_OUTLINE,leftbox, topbox,rightbox, bottombox,

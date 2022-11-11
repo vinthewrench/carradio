9116c1b038e344c52652a658d77594135b08f25a
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-11-11 13:50:47 -0800

    no message

--- src/CANBusMgr.cpp
+++ src/CANBusMgr.cpp
@@ -195,8 +195,8 @@ void CANBusMgr::processISOTPFrame(string ifName, can_frame_t frame, unsigned lon
 		
 		if(REQ){
 		
-			vector<uint8_t> bytes(frame.data,frame.data+len);
-	 
+			vector<uint8_t> bytes(frame.data+1,frame.data+len);
+ 		
 			{
 				printf("rcv  %03x [%2d] ", can_id, (int) len );
 				for(int i = 0; i < len; i++) printf("%02x ", bytes[i]);
